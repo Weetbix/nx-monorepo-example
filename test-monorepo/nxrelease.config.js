c30ee50944f812042c9604a0eda0cca1e355e4fe
John Hannagan <weetbix_@hotmail.com>
2023-03-06 22:46:29 +1100

    Move common things out of sub-project config

---
+++ test-monorepo/nxrelease.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  changelog: true,
+  npm: true,
+  github: false,
+  repositoryUrl: 'https://github.com/Weetbix/nx-monorepo-example',
+  branches: ['main'],
+  preset: 'conventionalcommits',
+};
