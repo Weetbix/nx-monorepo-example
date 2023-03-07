325ead4ee8ae99f9281247f8e655f38ac7c4661e
John Hannagan <weetbix_@hotmail.com>
2023-03-07 23:17:26 +1100

    use custom release rules

--- test-monorepo/nxrelease.config.js
+++ test-monorepo/nxrelease.config.js
@@ -5,4 +5,12 @@ module.exports = {
   repositoryUrl: 'https://github.com/Weetbix/nx-monorepo-example',
   branches: ['main'],
   commitMessage: 'chore(${PROJECT_NAME}): release version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+  releaseRules: [
+    {breaking: true, release: 'major'},
+    {revert: true, release: 'patch'},
+    {type: 'custom', release: 'minor'},
+    {type: 'feat', release: 'minor'},
+    {type: 'fix', release: 'patch'},
+    {type: 'perf', release: 'patch'},
+  ]
 };
