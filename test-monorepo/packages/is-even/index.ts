1a6a24085760c58b9ba019705fd35b04de01f3be
Weetbix <weetbix_@hotmail.com>
2023-03-08 22:29:07 +1100

    fix: touch all projects (#5)

--- test-monorepo/packages/is-even/index.ts
+++ test-monorepo/packages/is-even/index.ts
@@ -3,7 +3,7 @@ export const isEven = (x: number) => x % 2 == 0;
 export const isEven2 = (x: number) => x % 2 == 0;
 
 
-export const isEven2 = (x: number) => x % 2 == 0;
+export const isEven3 = (x: number) => x % 2 == 0;
 
 
 
