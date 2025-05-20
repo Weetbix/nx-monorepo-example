cdfb81698db5927e67c0ddeffcaac2ef1f68c989
John Hannagan <weetbix_@hotmail.com>
2025-05-20 11:56:26 +1000

    feat: use local packages

--- test-monorepo/packages/is-odd/index.ts
+++ test-monorepo/packages/is-odd/index.ts
@@ -1,6 +1,10 @@
+import { isEven } from "@weetbix/is-even";
+
 export const isOdd = (x: number) => x % 2 !== 0;
 
 export const isOdd2 = (x: number) => x % 2 !== 0;
+
+export const isOdd3 = (x: number) => !isEven(x);
 
 
 
